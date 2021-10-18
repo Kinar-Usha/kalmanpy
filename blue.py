c1313c2af21c31e4519b6ad52cae3abae01d4158
Kinar-Usha <kinar.cool@gmail.com>
2021-10-18 15:32:32 +0530

    something

--- blue.py
+++ blue.py
@@ -1,6 +1,7 @@
 from  bluepy.btle import Scanner
+import kalman as KalmanFilter
 scanner = Scanner()
-devices=scanner.scan(10.0)
-
+devices=scanner.scan(5.0)
+kalmanFilter = KalmanFilter(0.01,3)
 for device in devices:
-    print ("DEV={} RSSI ={}".format(device.addr,device.rssi))
+    print ("DEV={} RSSI ={}".format(device.addr,kalmanFilter.filter(device.rssi)))
